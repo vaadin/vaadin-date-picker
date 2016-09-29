10cddd68ceeee6f90d820bf3b9fefd0e5eacb42b
Anton Platonov <anton@vaadin.com>
2016-09-29 17:20:16 +0300

    Add screen readers support

--- test/common.js
+++ test/common.js
@@ -8,6 +8,7 @@ function getDefaultI18n() {
       'January', 'February', 'March', 'April', 'May',
       'June', 'July', 'August', 'September', 'October', 'November', 'December'
     ],
+    weekdays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
     weekdaysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
     firstDayOfWeek: 0,
     today: 'Today',
