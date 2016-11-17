d1a356ec0fc325769f2b4a768514518a97bca2ba
Tomi Virkki <virkki@vaadin.com>
2016-11-17 12:55:01 +0200

    Test on newer Chrome/FF versions

--- gulpfile.js
+++ gulpfile.js
@@ -68,8 +68,8 @@ function testSauce(browsers, done) {
 
 gulp.task('test:desktop', function(done) {
   testSauce([
-    'Windows 10/chrome@48',
-    'Windows 10/firefox@44',
+    'Windows 10/chrome@50',
+    'Windows 10/firefox@47',
     'Windows 10/microsoftedge@13',
     'Windows 10/internet explorer@11',
     'OS X 10.11/safari@9.0'], done);
