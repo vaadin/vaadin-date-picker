b982b3957e19e14e86d6862c24effe5c0135c322
Anton Platonov <anton@vaadin.com>
2017-05-30 13:09:28 +0300

    Try running full test suite on Travis

--- wct.conf.js
+++ wct.conf.js
@@ -1,10 +1,8 @@
 module.exports = {
   registerHooks: function(context) {
     context.options.plugins.sauce.browsers = [
-      // - So much random timeouts in CI but working in local
-      // 'OS X 10.11/iphone@10.2',
-      // 'OS X 10.11/ipad@10.2',
-
+      'OS X 10.11/iphone@10.2',
+      'OS X 10.11/ipad@10.2',
       'Windows 10/internet explorer@11',
       'Windows 10/chrome@58',
       'Windows 10/firefox@53',
